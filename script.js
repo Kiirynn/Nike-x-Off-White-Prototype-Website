d7dd38406822cf0603578abcd395e282d1af63fb
Kiirynn <kieran.poropat@gmail.com>
2023-01-13 17:30:44 -0500

    early build, I still suck at javascript and am rusty with css

---
+++ script.js
@@ -0,0 +1,24 @@
+const page1 = document.getElementById("page1");
+const page = document.getElementById("page");
+const active = document.getElementById("active")
+const fader = document.querySelectorAll('page2');
+
+
+
+
+
+window.addEventListener('scroll', () => {
+
+
+
+    //    let currentPage = window;
+    // if(currentPage){
+    //     page.classList.add('active');
+    // }
+})
+
+// function display(){
+//     active = display
+// }
+
+ 
