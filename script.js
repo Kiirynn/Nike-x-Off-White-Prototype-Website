ba7d81789f9c9d78680dc19ce4121ae2a5c84628
Kiirynn <kieran.poropat@gmail.com>
2023-01-18 15:32:15 -0500

    still trying to get shoe nav to work

--- script.js
+++ script.js
@@ -48,7 +48,21 @@ faders.forEach(fader => {
 });
 
 
+// <img class="shoe" id="shoe4"  src="Content/snkrs-special-off-white-dunk-5-e1623680402966.png" alt="">
+// <img class="shoe" id="shoe2"  src="Content/shoe 2 flip.png" alt="">
+// <img class="shoe" id="shoe5"  src="Content/Vigil-Nike-Inline02-GQ-07022019_3x2 trans.png" alt="">
+// <img class="shoe" id="shoe6"  src="Content/blue nike ow sneaker.png" alt=""> 
+
+
+
+
+
+
+
+
+
 const shoes = document.querySelector('.shoes');
+const shoe = document.querySelector('.shoe');
 const prevArrow = document.querySelector('.prev-arrow');
 const nextArrow = document.querySelector('.forward-arrow');
 const sneakerImages = ['Content/shoe 1.png','Content/snkrs-special-off-white-dunk-5-e1623680402966.png','Content/shoe 2 flip.png','Content/Vigil-Nike-Inline02-GQ-07022019_3x2 trans.png', 'Content/blue nike ow sneaker.png' ];
@@ -57,21 +71,42 @@ let i = 0;
 
 
 
+// shoe.addEventListener('click', () => {
+//       shoe.classList.toggle('active')
+// })
 
 
 
 
-prevArrow.addEventListener('click', () => {
-     if(prevArrow){
-        i++
-     }
+prevArrow.addEventListener('click', (e) => {
+    e.style.animation = `shoe transition: 750ms ease-in`;
+    if(i <= 0)i = sneakerImages.length;
+    
+    i--;
+    
 
-     shoes.image.classList.toggle('active')
+    return setImg();
+        
+      
+    
 })
 
 nextArrow.addEventListener('click', () => {
-
+    
+    if(i > sneakerImages.length -1);
+    i++;
+    return setImg();
+        
+        
+         
+    
 })
+
+
+function setImg(){
+    return shoe.setAttribute('src', sneakerImages[i]);
+    
+}
 
 
 
